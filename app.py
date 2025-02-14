475789f32a7dffce82bc7b00d3bed846b8224f3a
Nguyễn Công Cường <148767872+congcuongca1207@users.noreply.github.com>
2025-02-14 15:08:02 +0000

    MCI test

--- app.py
+++ app.py
@@ -1,43 +1,38 @@
-from flask import Flask, request, render_template
 import joblib
-import numpy as np
-
-app = Flask(__name__)
+from flask import Flask, render_template, request
+from sklearn.feature_extraction.text import CountVectorizer
 
 # Tải mô hình đã lưu
-model = joblib.load('model/logistic_regression_model.pkl')
+model = joblib.load('model/best_sentiment_model.pkl')
 
+# Tải vectorizer (nếu bạn đã lưu nó trước đó, nếu không, bạn sẽ phải huấn luyện lại nó)
+vectorizer = joblib.load('model/vectorizer.pkl')  # Hoặc tạo lại nếu cần
+
+# Khởi tạo Flask app
+app = Flask(__name__)
+
+# Trang chủ
 @app.route('/')
 def home():
-    return render_template('index.html', prediction=None)
+    return render_template('index.html')
 
+# Trang dự đoán cảm xúc
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Lấy dữ liệu từ biểu mẫu
-        feature1 = float(request.form['loan_amnt'])
-        feature2 = float(request.form['term'])  # Được giữ nguyên dưới dạng chuỗi
-        feature3 = float(request.form['int_rate'])
-        feature4 = float(request.form['installment'])
-        feature5 = float(request.form['grade'])  # Được giữ nguyên dưới dạng chuỗi
-        feature6 = float(request.form['emp_length'])  # Được giữ nguyên dưới dạng chuỗi
-        feature7 = float(request.form['home_ownership'])  # Được giữ nguyên dưới dạng chuỗi
-        feature8 = float(request.form['annual_inc'])
-        feature9 = float(request.form['dti'])
-        feature10 = float(request.form['delinq_2yrs'])
-        feature11 = float(request.form['fico_range_low'])
-        feature12 = float(request.form['fico_range_high'])
-    except ValueError:
-        return render_template('index.html', prediction='Dữ liệu đầu vào không hợp lệ.')
-
-    # Chuyển đổi dữ liệu thành numpy array
-    features_array = np.array([feature1, feature2, feature3, feature4, feature5, feature6, feature7, feature8, feature9, feature10, feature11, feature12]).reshape(1, -1)
+    # Lấy văn bản người dùng nhập
+    text = request.form['text']
     
-    # Dự đoán với mô hình đã tải
-    prediction = model.predict(features_array)[0]
+    # Biến đổi văn bản đầu vào
+    text_vectorized = vectorizer.transform([text])
     
-    # Trả về kết quả dự đoán
-    return render_template('index.html', prediction=float(prediction))
+    # Dự đoán
+    prediction = model.predict(text_vectorized)
+    
+    # Chuyển đổi kết quả dự đoán thành nhãn cảm xúc
+    sentiment = ['Negative', 'Neutral', 'Positive']
+    result = sentiment[prediction[0]]
+    
+    return render_template('index.html', prediction_text='Sentiment: {}'.format(result))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
